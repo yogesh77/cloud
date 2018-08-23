7f976bbf719ffdc0a4ab5d11c452beeda6edca2c
Yogesh Aggarwal <yogeshaggarwal25@gmail.com>
2018-08-23 09:30:48 +0530

    Commit

--- src/app/email/email.component.ts
+++ src/app/email/email.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { LoginService } from '../login/login.service';
 
 @Component({
   selector: 'app-email',
@@ -6,15 +7,55 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./email.component.css']
 })
 export class EmailComponent implements OnInit {
-	display: boolean = false;
+	userdata: any [];
+	display: boolean = false;	
+	userlist: any[];
+	userdetails: any;
+	userList: any = [];
+	authorizedUserDetails:string;
+	addRecordError: any;
 
-  constructor() { }
+  constructor(private loginService: LoginService) {
+  	this.userlist = [];
+  	this.loginService.showuserauthorization()
+  	.then((res) => {
+  		this.userdetails = JSON.parse(res._body);
+  		
+     	 this.userdata = this.userdetails.authorization;
+     	 for (let key in this.userdata[0]){
+     	 	this.userlist.push(key);
+     	 }
 
-  addAuthorisedUser(){
+  		
+  		// console.log("userdata",this.userdata);
+  	})
+
+
+   }
+
+  showDialog(){
   	this.display = true;
   }
 
+  focus() {
+    this.addRecordError = '';
+  }
+
+  submituserdetails(){
+  	
+  	// this.email =this.authorizedUserDetails.emailAddress
+  	this.loginService.adduserauthorization(this.authorizedUserDetails)
+  	// .then (() => this.onAddRecordSuccess(recordType))
+  	// .catch((error) => this.addRecordError = error._body);
+  }
+
+  // onAddRecordSuccess(recordType: string)){
+  // 	this.authorizedUserDetails.emailAddress = '';
+  // }
+
   ngOnInit() {
+
+  	
   }
 
 }

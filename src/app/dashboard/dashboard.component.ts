8f3ffdbdec9794bb11d42a1c1c3936a8494e8698
unknown <yogeshaggarwal25@gmail.com>
2018-09-02 21:26:46 +0530

    Commit

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -6,7 +6,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dashboard.component.scss']
 })
 
-export class DashboardComponent {
+export class DashboardComponent implements OnInit{
 
-	
+	itemCount: number = 4;
+
+	 btnText: string = 'Add an Item';    // Add this line
+
+	 goalText: string = 'My first life goal';    // Add this
+
+	 goals = [];
+
+	 ngOnInit() {
+    this.itemCount = this.goals.length;
+
+	 console.log(this.itemCount);
+  }
+
+addItem() {
+    this.goals.push(this.goalText);
+    this.goalText = '';
+    this.itemCount = this.goals.length;
+     console.log(this.itemCount);
+  }
+  }
+
 }
